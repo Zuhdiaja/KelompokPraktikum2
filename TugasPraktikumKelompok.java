603c7bb18069e94ad9feb56a880abc1bdda1fba6
zuhdi <zuhdiyuda21@gmail.com>
2024-09-10 14:48:15 +0700

    commit pertama

---
+++ TugasPraktikumKelompok.java
@@ -0,0 +1,18 @@
+/**
+ * TugasPraktikumKelompok
+ */
+public class TugasPraktikumKelompok {
+    public static void main(String[] args) {
+        System.out.print("---------------------------------------------------------");
+        System.out.print("\n-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.print("\n---------------------------------------------------------");
+        System.out.print("\nNama    : Muhammad Zuhdi Yudadharma");
+        System.out.print("\nNIM     : 244107020017");
+        System.out.print("\nKelas   : 1G");
+        System.out.print("\nNo Absen: 20 ");
+        System.out.print("\nProdi   : D-IV Teknik Informatika");
+        System.out.print("\nAlamat  : Jl Semanggi Barat");
+        System.out.print("\nNo HP   : 085608797716");
+        System.out.print("\nHobi    :tidur");
+    }
+}
