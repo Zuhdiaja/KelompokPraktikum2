d5eef2c5fe46206c911d398bb4064d1fb4e8375c
faiqnabil <fq28nabil@gmail.com>
2024-09-10 15:07:09 +0700

    commit faiq

--- TugasPraktikumKelompok.java
+++ TugasPraktikumKelompok.java
@@ -6,13 +6,13 @@ public class TugasPraktikumKelompok {
         System.out.print("---------------------------------------------------------");
         System.out.print("\n-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
         System.out.print("\n---------------------------------------------------------");
-        System.out.print("\nNama    : Muhammad Zuhdi Yudadharma");
-        System.out.print("\nNIM     : 244107020017");
+        System.out.print("\nNama    : Muhammad Faiq Nabil Saputra");
+        System.out.print("\nNIM     : 244107020025");
         System.out.print("\nKelas   : 1G");
-        System.out.print("\nNo Absen: 20 ");
+        System.out.print("\nNo Absen: 17 ");
         System.out.print("\nProdi   : D-IV Teknik Informatika");
-        System.out.print("\nAlamat  : Jl Semanggi Barat");
-        System.out.print("\nNo HP   : 085608797716");
-        System.out.print("\nHobi    :tidur");
+        System.out.print("\nAlamat  : Jl Musi nomor 15 Kota Blitar");
+        System.out.print("\nNo HP   : 0881026153299");
+        System.out.print("\nHobi    : Healling");
     }
 }
